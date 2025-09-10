3750bb52d48e6878548336c59883243eba9f64ba
Vaidehi Patel <vpatel278@myseneca.ca>
2025-09-10 17:13:33 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+# Name: Vaidehi Patel
+# OPS 445 NDD
+# Std ID: 166249219
+
+# Lab 1 Script 3 - Lab 1 c
+
+#num1 = 5
+#num2 = 10
+
+#print(num1)
+#print(num2)
+
+#sum = num1 + num2
+
+#print(sum)
+
+#print('The sum is: ' + str(sum))
+
+name = 'Isaac' 
+
+#age: int   # I learnt about this ":" declaration on google, but I did not use it because I later learnt that this is used to just hint an IDE that age is int, but Python would not enforce this declaration in this case. This was just for my learning.
+
+age = int(72)   # Here Python would convert(force) the value to int.
+
+print(name + ' is ' + str(age) + ' years old!')
