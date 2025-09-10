95c085d9f13f4fa3d38c73ead45d5392fcab58ae
Vaidehi Patel <vpatel278@myseneca.ca>
2025-09-10 16:49:02 -0400

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+#Name: Vaidehi Patel
+#OPS445 NDD
+#Std ID: 166249219
+
+#Lab1 Script 2
+
+#name = 'Thomas'
+name = "Isaac"
+
+#print(name)
+
+#print('name')
+
+#print('I have a friend named ' + name)
+print('How old are you ' + name + '?')
