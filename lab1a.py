98f174c8f1e63331b4637993c315f1f41bc3a0d9
Vaidehi Patel <vpatel278@myseneca.ca>
2025-09-10 16:29:06 -0400

    Completed 1st part of the lab which is lab1a and trying and learning to commit and push.

---
+++ lab1a.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''Description: This program will output "hello world" to the screen.'''
+
+#This is my first Python file
+#Name: Vaidehi Patel
+#Std ID: 166249219
+
+print('Hello world')
